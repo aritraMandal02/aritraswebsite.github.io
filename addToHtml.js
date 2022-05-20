1c3ec3ff1b06897f19c8d8903d1bc311de177818
Aritra Mandal <aritra.mandal2002@gmail.com>
2022-05-21 01:41:27 +0530

    update

---
+++ addToHtml.js
@@ -0,0 +1,12 @@
+function myFunc(){
+    alert("Working!");
+}
+function saydate(){
+    alert(Date())
+}
+function changetext(){
+    document.getElementById("text").innerHTML="This is change";
+}
+function disappear(){
+    document.getElementById("disappear").style.display="none";
+}
